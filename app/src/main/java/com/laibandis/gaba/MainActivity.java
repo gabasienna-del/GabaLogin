72d89463bfda06d9cfa25d58371c85549726fd99
gabasienna-del <gabasienna@gmail.com>
2026-01-03 20:51:45 +0500

    github stable apk

---
+++ app/src/main/java/com/laibandis/gaba/MainActivity.java
@@ -0,0 +1,12 @@
+package com.laibandis.gaba;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(android.R.layout.simple_list_item_1);
+    }
+}

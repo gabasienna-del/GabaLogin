ec87a3b17a9519b63e1f1b9ab418ab158d46b773
gabasienna-del <gabasienna@gmail.com>
2026-01-03 23:21:31 +0500

    final stable android11+ build

--- app/src/main/java/com/laibandis/gaba/App.kt
+++ app/src/main/java/com/laibandis/gaba/App.kt
@@ -1,5 +1,11 @@
 package com.laibandis.gaba
 
 import android.app.Application
+import androidx.multidex.MultiDex
 
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MultiDex.install(this)
+    }
+}

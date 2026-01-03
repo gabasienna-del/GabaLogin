16b49866e7c183a3a8e0f6a3c46c7a8100577203
gabasienna-del <gabasienna@gmail.com>
2026-01-03 06:02:41 +0500

    Clean final MainActivity for OkHttp 5.3.0

--- app/src/main/java/com/laibandis/gaba/MainActivity.kt
+++ app/src/main/java/com/laibandis/gaba/MainActivity.kt
@@ -1,77 +1,88 @@
 package com.laibandis.gaba
 
-import android.os.*
+import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import java.security.MessageDigest
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
- lateinit var phone:EditText
- lateinit var code:EditText
- lateinit var log:TextView
- var authId=""
+    lateinit var phone: EditText
+    lateinit var code: EditText
+    lateinit var log: TextView
+    var authId = ""
 
- val CAS="https://cas-gw-cf.euce1.indriverapp.com"
- val APP_ID="com.laibandis.gaba"
+    val CAS = "https://cas-gw-cf.euce1.gabaapp.com"
+    val APP_ID = "com.laibandis.gaba"
 
- override fun onCreate(b:Bundle?){
-  super.onCreate(b)
-  setContentView(R.layout.activity_main)
+    override fun onCreate(b: Bundle?) {
+        super.onCreate(b)
+        setContentView(R.layout.activity_main)
 
-  phone=findViewById(R.id.phone)
-  code=findViewById(R.id.code)
-  log=findViewById(R.id.log)
+        phone = findViewById(R.id.phone)
+        code  = findViewById(R.id.code)
+        log   = findViewById(R.id.log)
 
-  findViewById<Button>(R.id.send).setOnClickListener{requestCode()}
-  findViewById<Button>(R.id.check).setOnClickListener{checkCode()}
- }
+        findViewById<Button>(R.id.send).setOnClickListener { requestCode() }
+        findViewById<Button>(R.id.check).setOnClickListener { checkCode() }
+    }
 
- fun deviceId()=System.currentTimeMillis().toString()+"-"+Random.nextLong()
- fun md5(s:String)=MessageDigest.getInstance("MD5").digest(s.toByteArray())
-  .joinToString(""){"%02x".format(it)}.substring(0,16)
+    fun deviceId() = System.currentTimeMillis().toString() + "-" + Random.nextLong()
 
- fun requestCode(){
-  val dev=deviceId()
-  val androidId=md5(dev)
-  val j=JSONObject().apply{
-   put("phone",phone.text.toString())
-   put("locale","ru")
-   put("platform","android")
-   put("app_id",APP_ID)
-   put("device_id",dev)
-   put("android_id",androidId)
-   put("fingerprint",JSONObject().apply{
-    put("model","SM-N970F"); put("brand","samsung"); put("os",30); put("tz","+05:00")
-   })
-  }
-  post("$CAS/api/authorization",j){
-   authId=it.getJSONObject("response").getJSONArray("items").getJSONObject(0).getString("auth_id")
-   log.text="Код отправлен\n$authId"
-  }
- }
+    fun md5(s: String): String =
+        MessageDigest.getInstance("MD5").digest(s.toByteArray())
+            .joinToString("") { "%02x".format(it) }.substring(0, 16)
 
- fun checkCode(){
-  val j=JSONObject().apply{
-   put("auth_id",authId)
-   put("code",code.text.toString())
-   put("app_id",APP_ID)
-   put("platform","android")
-  }
-  post("$CAS/api/v2/checkauthcode",j){ log.text=it.toString(2) }
- }
+    fun requestCode() {
+        val dev = deviceId()
+        val androidId = md5(dev)
 
- fun post(url:String, body:JSONObject, cb:(JSONObject)->Unit){
-  Thread{
-   val r=Net.client.newCall(
-    Request.Builder().url(url)
-     .post(body.toString().toRequestBody("application/json".toMediaType()))
-     .build()
-   ).execute().body!!.string()
-   runOnUiThread{cb(JSONObject(r))}
-  }.start()
- }
+        val j = JSONObject().apply {
+            put("phone", phone.text.toString())
+            put("locale", "ru")
+            put("platform", "android")
+            put("app_id", APP_ID)
+            put("device_id", dev)
+            put("android_id", androidId)
+            put("fingerprint", JSONObject().apply {
+                put("model", "SM-N970F")
+                put("brand", "samsung")
+                put("os", 30)
+                put("tz", "+05:00")
+            })
+        }
+
+        post("$CAS/api/authorization", j) {
+            authId = it.getJSONObject("response")
+                .getJSONArray("items")
+                .getJSONObject(0)
+                .getString("auth_id")
+            log.text = "Код отправлен\n$authId"
+        }
+    }
+
+    fun checkCode() {
+        val j = JSONObject().apply {
+            put("auth_id", authId)
+            put("code", code.text.toString())
+            put("app_id", APP_ID)
+            put("platform", "android")
+        }
+        post("$CAS/api/v2/checkauthcode", j) { log.text = it.toString(2) }
+    }
+
+    fun post(url: String, body: JSONObject, cb: (JSONObject) -> Unit) {
+        Thread {
+            val reqBody = body.toString().toRequestBody("application/json".toMediaType())
+            val r = Net.client.newCall(
+                Request.Builder().url(url).post(reqBody).build()
+            ).execute().body!!.string()
+            runOnUiThread { cb(JSONObject(r)) }
+        }.start()
+    }
 }
